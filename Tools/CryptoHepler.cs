93c64843cb41d3e41656f5f9e55bc1e1d082b5e1
delafqm <delafqm@163.com>
2018-12-13 11:45:48 +0800

    1

---
+++ Tools/CryptoHepler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tools
+{
+    /// <summary>
+    /// 加密解密算法
+    /// </summary>
+    public class CryptoHepler
+    {
+    }
+}
